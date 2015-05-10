fa1ba1f22c481ec00e0c84da03cf3277f0d7dc4f
alinatsikhanava <alina.tsikhanava@gmail.com>
2015-05-11 00:16:45 +0200

    initial versions

---
+++ plot1.R
@@ -0,0 +1,7 @@
+dat <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+dat$Time <- strptime(dat$Time, "%H:%M:%S")
+dat$Date <- as.Date(dat$Date, format = "%d/%m/%Y")
+tidydat <- subset(dat, dat$Date == "2007-02-01" | dat$Date == "2007-02-02")
+hist(tidydat$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot1.png")
+dev.off()
